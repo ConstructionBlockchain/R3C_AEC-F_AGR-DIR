cb16d1f2a3df69008e588f764d32d01b2cfbde07
Xavier Lepretre <xavier.lepretre@gmail.com>
2018-10-24 20:03:04 +0100

    Added further checks on FlowResponder.

--- src/main/kotlin/com/cordacodeclub/directAgreement/AgreementFlowResponder.kt
+++ src/main/kotlin/com/cordacodeclub/directAgreement/AgreementFlowResponder.kt
@@ -1,11 +1,13 @@
 package com.cordacodeclub.directAgreement
 
 import co.paralleluniverse.fibers.Suspendable
+import com.cordacodeclub.directAgreement.DirectAgreementContract.Companion.ID
 import net.corda.core.contracts.requireThat
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.FlowSession
 import net.corda.core.flows.InitiatedBy
 import net.corda.core.flows.SignTransactionFlow
+import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 
 //Define LegalAgreementFlowResponder
@@ -16,9 +18,13 @@ class AgreementFlowResponder(val otherPartySession: FlowSession) : FlowLogic<Uni
         val signTransactionFlow = object : SignTransactionFlow(otherPartySession, SignTransactionFlow.tracker()) {
             override fun checkTransaction(stx: SignedTransaction) = requireThat {
                 val output = stx.tx.outputs.single().data
-                "This must be an LegalAgreement transaction." using (output is LegalAgreementState)
+                "This must be an LegalAgreement state." using (output is LegalAgreementState)
                 val legalAgreement = output as LegalAgreementState
                 "The legal agreement's value can't be negative." using (legalAgreement.value.quantity > 0)
+                "The intermediary should be the sender." using (legalAgreement.intermediary == otherPartySession.counterparty)
+                "PartyA should be me." using (legalAgreement.partyA == ourIdentity)
+                val contract = stx.tx.outputs.single().contract
+                "This must be a DirectAgreementContract." using (contract == ID)
             }
         }
         subFlow(signTransactionFlow)
