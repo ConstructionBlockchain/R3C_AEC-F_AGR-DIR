3de723d8ac9c3d4da08bcc5361f0da01bc324598
Xavier Lepretre <xavier.lepretre@gmail.com>
2018-10-24 21:34:01 +0100

    Put the initiator and responder flows into their own object.

--- src/main/kotlin/com/cordacodeclub/directAgreement/DirectAgreementFlow.kt
+++ src/main/kotlin/com/cordacodeclub/directAgreement/DirectAgreementFlow.kt
@@ -1,66 +1,92 @@
 package com.cordacodeclub.directAgreement
 
 import co.paralleluniverse.fibers.Suspendable
-import com.cordacodeclub.directAgreement.DirectAgreementContract.Companion.ID
 import net.corda.core.contracts.Command
+import net.corda.core.contracts.Requirements.using
 import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
-import net.corda.core.identity.Party
+import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 import java.lang.IllegalArgumentException
 
-@InitiatingFlow
-@StartableByRPC
-/**
- * This flow can be started by partyA or partyB.
- * And it has to be signed by partyA and partyB.
- */
-class DirectAgreementFlow(val inputStateAndRef: StateAndRef<LegalAgreementState>) : FlowLogic<Unit>() {
+object DirectAgreementFlow {
 
-    /** The progress tracker provides checkpoints indicating the progress of the flow to observers. */
-    override val progressTracker = ProgressTracker()
+    @InitiatingFlow
+    @StartableByRPC
+    /**
+     * This flow can be started by partyA or partyB.
+     * And it has to be signed by partyA and partyB.
+     */
+    class DirectAgreementFlowInitiator(val inputStateAndRef: StateAndRef<LegalAgreementState>) : FlowLogic<Unit>() {
 
-    /** The flow logic is encapsulated within the call() method. */
-    @Suspendable
-    override fun call() {
-        // We retrieve the first notary identity from the network map.
-        val notary = serviceHub.networkMapCache.notaryIdentities[0]
+        /** The progress tracker provides checkpoints indicating the progress of the flow to observers. */
+        override val progressTracker = ProgressTracker()
 
-        // We create the transaction output state from the input.
-        val inputState = inputStateAndRef.state.data
-        val outputState = inputState.copy(status = LegalAgreementState.Status.DIRECT)
+        /** The flow logic is encapsulated within the call() method. */
+        @Suspendable
+        override fun call() {
+            // We retrieve the first notary identity from the network map.
+            val notary = serviceHub.networkMapCache.notaryIdentities[0]
 
-        val cmd = Command(DirectAgreementContract.Commands.GoToDirect(),
-                listOf(inputState.partyA.owningKey, inputState.partyB.owningKey))
+            // We create the transaction output state from the input.
+            val inputState = inputStateAndRef.state.data
+            val outputState = inputState.copy(status = LegalAgreementState.Status.DIRECT)
 
-        val txBuilder = TransactionBuilder(notary = notary)
+            val cmd = Command(DirectAgreementContract.Commands.GoToDirect(),
+                    listOf(inputState.partyA.owningKey, inputState.partyB.owningKey))
 
-        // We add the items ot the builder
-        txBuilder.addOutputState(outputState, ID)
-        txBuilder.addCommand(cmd)
-        txBuilder.addInputState(inputStateAndRef)
+            val txBuilder = TransactionBuilder(notary = notary)
 
-        // Verifying the transaction
-        txBuilder.verify(serviceHub)
+            // We add the items ot the builder
+            txBuilder.addOutputState(outputState, DirectAgreementContract.ID)
+            txBuilder.addCommand(cmd)
+            txBuilder.addInputState(inputStateAndRef)
 
-        // Signing the transaction
-        val signedTx = serviceHub.signInitialTransaction(txBuilder)
+            // Verifying the transaction
+            txBuilder.verify(serviceHub)
 
-        // Creating a session with the other party
-        val otherParty = if (ourIdentity == inputState.partyA)
-            inputState.partyB
+            // Signing the transaction
+            val signedTx = serviceHub.signInitialTransaction(txBuilder)
+
+            // Creating a session with the other party
+            val otherParty = if (ourIdentity == inputState.partyA)
+                inputState.partyB
             else (if (ourIdentity == inputState.partyB)
                 inputState.partyA
-                else throw IllegalArgumentException("Unexpected party"))
-        val otherPartySession = initiateFlow(otherParty)
+            else throw IllegalArgumentException("Unexpected party"))
+            val otherPartySession = initiateFlow(otherParty)
 
-        // Obtaining the counterparty's signature
-        val fullySignedTx = subFlow(CollectSignaturesFlow(
-                signedTx,
-                listOf(otherPartySession), CollectSignaturesFlow.tracker()))
+            // Obtaining the counterparty's signature
+            val fullySignedTx = subFlow(CollectSignaturesFlow(
+                    signedTx,
+                    listOf(otherPartySession), CollectSignaturesFlow.tracker()))
 
-        // We finalise the transaction with the notary.
-        subFlow(FinalityFlow(fullySignedTx))
+            // We finalise the transaction with the notary.
+            subFlow(FinalityFlow(fullySignedTx))
+        }
+    }
+
+    /**
+     * Define LegalAgreementFlowResponder
+     * We want to be able to call this flow only when we confirm that we are expecting it.
+     */
+    @InitiatedBy(DirectAgreementFlowInitiator::class)
+    class DirectAgreementFlowResponder(val otherPartySession: FlowSession,
+                                       val isOk: Boolean) : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call() {
+            val signTransactionFlow = object : SignTransactionFlow(otherPartySession, SignTransactionFlow.tracker()) {
+                override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                    "This flow must have been approved" using (isOk)
+
+                    // How to confirm that this builds on an input we agreed on and of which we are party
+
+                    // Do we need to check that the output "matches" the input as if the tx was verified?
+                }
+            }
+            subFlow(signTransactionFlow)
+        }
     }
 }
--- src/main/kotlin/com/cordacodeclub/directAgreement/LegalAgreementFlow.kt
+++ src/main/kotlin/com/cordacodeclub/directAgreement/LegalAgreementFlow.kt
@@ -5,60 +5,96 @@ import com.cordacodeclub.directAgreement.DirectAgreementContract.Commands.Create
 import com.cordacodeclub.directAgreement.DirectAgreementContract.Companion.ID
 import net.corda.core.contracts.Amount
 import net.corda.core.contracts.Command
+import net.corda.core.contracts.Requirements.using
+import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
 import net.corda.core.identity.Party
+import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 import java.util.*
 
-@InitiatingFlow
-@StartableByRPC
-/**
- * This flow is to be started by the intermediary.
- */
-class LegalAgreementFlow(val agreementValue: Amount<Currency>,
-                         val partyA: Party,
-                         val partyB: Party) : FlowLogic<Unit>() {
+object LegalAgreementFlow {
 
-    /** The progress tracker provides checkpoints indicating the progress of the flow to observers. */
-    override val progressTracker = ProgressTracker()
+    @InitiatingFlow
+    @StartableByRPC
+    /**
+     * This flow is to be started by the intermediary.
+     */
+    class LegalAgreementFlowInitiator(val agreementValue: Amount<Currency>,
+                                      val partyA: Party,
+                                      val partyB: Party) : FlowLogic<Unit>() {
 
-    /** The flow logic is encapsulated within the call() method. */
-    @Suspendable
-    override fun call() {
-        // We retrieve the first notary identity from the network map.
-        val notary = serviceHub.networkMapCache.notaryIdentities[0]
+        /** The progress tracker provides checkpoints indicating the progress of the flow to observers. */
+        override val progressTracker = ProgressTracker()
 
-        // We create the transaction output.
-        val outputState = LegalAgreementState(
-                intermediary = ourIdentity,
-                partyA = partyA,
-                partyB = partyB,
-                status = LegalAgreementState.Status.INTERMEDIATE,
-                value = agreementValue)
-        val cmd = Command(Create(), listOf(ourIdentity.owningKey, partyA.owningKey))
+        /** The flow logic is encapsulated within the call() method. */
+        @Suspendable
+        override fun call() {
+            // We retrieve the first notary identity from the network map.
+            val notary = serviceHub.networkMapCache.notaryIdentities[0]
 
-        val txBuilder = TransactionBuilder(notary = notary)
+            // We create the transaction output.
+            val outputState = LegalAgreementState(
+                    intermediary = ourIdentity,
+                    partyA = partyA,
+                    partyB = partyB,
+                    status = LegalAgreementState.Status.INTERMEDIATE,
+                    value = agreementValue)
+            val cmd = Command(Create(), listOf(ourIdentity.owningKey, partyA.owningKey))
 
-        //We add the output state to the builder
-        txBuilder.addOutputState(outputState, ID)
-        txBuilder.addCommand(cmd)
+            val txBuilder = TransactionBuilder(notary = notary)
 
-        // Verifying the transaction
-        txBuilder.verify(serviceHub)
+            //We add the output state to the builder
+            txBuilder.addOutputState(outputState, ID)
+            txBuilder.addCommand(cmd)
 
-        // Signing the transaction
-        val signedTx = serviceHub.signInitialTransaction(txBuilder)
+            // Verifying the transaction
+            txBuilder.verify(serviceHub)
 
-        // Creating a session with the other party
-        val otherPartySession = initiateFlow(partyA)
+            // Signing the transaction
+            val signedTx = serviceHub.signInitialTransaction(txBuilder)
 
-        // Obtaining the counterparty's signature
-        val fullySignedTx = subFlow(CollectSignaturesFlow(
-                signedTx,
-                listOf(otherPartySession), CollectSignaturesFlow.tracker()))
+            // Creating a session with the other party
+            val otherPartySession = initiateFlow(partyA)
 
-        // We finalise the transaction with the notary.
-        subFlow(FinalityFlow(fullySignedTx))
+            // Obtaining the counterparty's signature
+            val fullySignedTx = subFlow(CollectSignaturesFlow(
+                    signedTx,
+                    listOf(otherPartySession), CollectSignaturesFlow.tracker()))
+
+            // We finalise the transaction with the notary.
+            subFlow(FinalityFlow(fullySignedTx))
+        }
+    }
+
+    /**
+     * Define LegalAgreementFlowResponder
+     * We want to be able to pass the expected parameters partyB and value so that we confirm we are not hoodwinked into
+     * accepting values that we do not agree with.
+     * As seen here https://docs.corda.net/flow-state-machines.html#a-two-party-trading-flow
+     */
+    @InitiatedBy(LegalAgreementFlowInitiator::class)
+    class LegalAgreementFlowResponder(val otherPartySession: FlowSession,
+                                      val partyB: Party,
+                                      val value: Long) : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call() {
+            val signTransactionFlow = object : SignTransactionFlow(otherPartySession, SignTransactionFlow.tracker()) {
+                override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                    val output = stx.tx.outputs.single().data
+                    "This must be a LegalAgreement state." using (output is LegalAgreementState)
+                    val legalAgreement = output as LegalAgreementState
+                    "The legal agreement's value can't be negative." using (legalAgreement.value.quantity > 0)
+                    "The legal agreement's value should be as expected." using (legalAgreement.value.quantity == value)
+                    "The intermediary should be the sender." using (legalAgreement.intermediary == otherPartySession.counterparty)
+                    "PartyA should be me." using (legalAgreement.partyA == ourIdentity)
+                    "PartyB should be as expected." using (legalAgreement.partyB == partyB)
+                    val contract = stx.tx.outputs.single().contract
+                    "This must be a DirectAgreementContract." using (contract == ID)
+                }
+            }
+            subFlow(signTransactionFlow)
+        }
     }
 }
