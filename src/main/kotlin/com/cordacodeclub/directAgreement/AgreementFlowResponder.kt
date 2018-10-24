c8a6b4e5f27f0cb2a5532dd57c65ca97aa3739fd
Xavier Lepretre <xavier.lepretre@gmail.com>
2018-10-24 20:03:26 +0100

    Identified that we want to integrate a human element to the verification.

--- src/main/kotlin/com/cordacodeclub/directAgreement/AgreementFlowResponder.kt
+++ src/main/kotlin/com/cordacodeclub/directAgreement/AgreementFlowResponder.kt
@@ -10,9 +10,14 @@ import net.corda.core.flows.SignTransactionFlow
 import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 
-//Define LegalAgreementFlowResponder
+/**
+ * Define LegalAgreementFlowResponder
+ * We want to be able to pass the expected parameters partyB and value so that we confirm we are not hoodwinked into
+ * accepting values that we do not agree with. This is a human interaction part and we understand that it is part of
+ * Corda's future plans.
+ */
 @InitiatedBy(LegalAgreementFlow::class)
-class AgreementFlowResponder(val otherPartySession: FlowSession) : FlowLogic<Unit>() {
+class AgreementFlowResponder(val otherPartySession: FlowSession/*, val partyB: Party, val value: Long*/) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
         val signTransactionFlow = object : SignTransactionFlow(otherPartySession, SignTransactionFlow.tracker()) {
@@ -21,8 +26,10 @@ class AgreementFlowResponder(val otherPartySession: FlowSession) : FlowLogic<Uni
                 "This must be an LegalAgreement state." using (output is LegalAgreementState)
                 val legalAgreement = output as LegalAgreementState
                 "The legal agreement's value can't be negative." using (legalAgreement.value.quantity > 0)
+//                "The legal agreement's value should be as expected." using (legalAgreement.value.quantity == value)
                 "The intermediary should be the sender." using (legalAgreement.intermediary == otherPartySession.counterparty)
                 "PartyA should be me." using (legalAgreement.partyA == ourIdentity)
+//                "PartyB should be as expected." using (legalAgreement.partyB == partyB)
                 val contract = stx.tx.outputs.single().contract
                 "This must be a DirectAgreementContract." using (contract == ID)
             }
