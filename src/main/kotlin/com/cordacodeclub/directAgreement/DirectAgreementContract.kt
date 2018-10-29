fcbc9b0088e07356dadef0d3bd96b5fd49eacb4d
Xavier Lepretre <xavier.lepretre@gmail.com>
2018-10-29 18:52:59 +0000

    Minor fixes on Contract.

--- src/main/kotlin/com/cordacodeclub/directAgreement/DirectAgreementContract.kt
+++ src/main/kotlin/com/cordacodeclub/directAgreement/DirectAgreementContract.kt
@@ -15,7 +15,6 @@ class DirectAgreementContract : Contract {
 
     fun verifyState(state: LegalAgreementState, signers: List<PublicKey>) {
         requireThat {
-
             "There must be only 2 signers" using (signers.toSet().size == 2)
             "Intermediary and partyA must be signers" using (signers.containsAll(listOf(
                     state.intermediary.owningKey, state.partyA.owningKey)))
@@ -25,47 +24,35 @@ class DirectAgreementContract : Contract {
     override fun verify(tx: LedgerTransaction) {
         val command = tx.commands.requireSingleCommand<Commands>()
         val inputCount = tx.inputsOfType<LegalAgreementState>().count()
-        requireThat {
-            val outputCount = tx.outputsOfType<LegalAgreementState>().count()
-            "There should be one output state of type LegalAgreementState" using (outputCount == 1)
-        }
+        val outputCount = tx.outputsOfType<LegalAgreementState>().count()
 
-        val output = tx.outputsOfType<LegalAgreementState>().single()
         requireThat {
             "There must be only 2 signers" using (command.signers.toSet().size == 2)
+            "There should be one output state of type LegalAgreementState" using (outputCount == 1)
         }
+        val output = tx.outputsOfType<LegalAgreementState>().single()
 
         when (command.value) {
-            is Commands.Create -> {
-                requireThat {
-                    "There should be no input of type LegalAgreementState when creating via Intermediary" using (inputCount == 0)
-                    "The output should have status INTERMEDIATE" using (output.status == LegalAgreementState.Status.INTERMEDIATE)
-                    "Intermediary and partyA must be signers" using (command.signers.containsAll(listOf(
-                            output.intermediary.owningKey, output.partyA.owningKey)))
-                }
+            is Commands.Create -> requireThat {
+                "There should be no input of type LegalAgreementState when creating via Intermediary" using (inputCount == 0)
+                "The output should have status INTERMEDIATE" using (output.status == LegalAgreementState.Status.INTERMEDIATE)
+                "Intermediary and partyA must be signers" using (command.signers.containsAll(listOf(
+                        output.intermediary.owningKey, output.partyA.owningKey)))
             }
 
-            is Commands.GoToDirect -> {
-                requireThat {
-                    "One LegalAgreementState should be consumed when creating Direct" using (inputCount == 1)
-                }
-
+            is Commands.GoToDirect -> requireThat {
+                "One LegalAgreementState should be consumed when creating Direct" using (inputCount == 1)
+                "There should be one output state of type LegalAgreementState" using (outputCount == 1)
                 val input = tx.inputsOfType<LegalAgreementState>().single()
-                requireThat {
-                    "The input should have status INTERMEDIATE" using (input.status == LegalAgreementState.Status.INTERMEDIATE)
-                    // Can we assume that the requirements in `Create` are fulfilled?
-                }
-
-                requireThat {
-                    "The output should have status DIRECT" using (output.status == LegalAgreementState.Status.DIRECT)
-                    "The output value should be equal to the input value" using (output.value == input.value)
-                    "The intermediary should be the same entity on both states" using (input.intermediary == output.intermediary)
-                    "The partyA should be the same entity on both states" using (input.partyA == output.partyA)
-                    "The partyB should be the same entity on both states" using (input.partyB == output.partyB)
-
-                    "PartyA and partyB must be signers" using (command.signers.containsAll(listOf(
-                            output.partyA.owningKey, output.partyB.owningKey)))
-                }
+                "The input should have status INTERMEDIATE" using (input.status == LegalAgreementState.Status.INTERMEDIATE)
+                // Can we assume that the requirements in `Create` are fulfilled?
+                "The output should have status DIRECT" using (output.status == LegalAgreementState.Status.DIRECT)
+                "The output value should be equal to the input value" using (output.value == input.value)
+                "The intermediary should be the same entity on both states" using (input.intermediary == output.intermediary)
+                "The partyA should be the same entity on both states" using (input.partyA == output.partyA)
+                "The partyB should be the same entity on both states" using (input.partyB == output.partyB)
+                "PartyA and partyB must be signers" using (command.signers.containsAll(listOf(
+                        output.partyA.owningKey, output.partyB.owningKey)))
             }
 
             is Commands.Finalise -> {
@@ -77,9 +64,6 @@ class DirectAgreementContract : Contract {
                 requireThat {
                     "The input should have status INTERMEDIATE or DIRECT" using
                             (input.status == LegalAgreementState.Status.INTERMEDIATE || input.status == LegalAgreementState.Status.DIRECT )
-                }
-
-                requireThat {
                     "The output should have status COMPLETED" using (output.status == LegalAgreementState.Status.COMPLETED)
                     "The output value should be equal to the input value" using (output.value == input.value)
                     "The intermediary should be the same entity on both states" using (input.intermediary == output.intermediary)
@@ -96,6 +80,7 @@ class DirectAgreementContract : Contract {
                     LegalAgreementState.Status.DIRECT -> requireThat {
                         "PartyA and PartyB must be the signers" using (command.signers.containsAll(listOf(output.partyA.owningKey, output.partyB.owningKey)))
                     }
+                    LegalAgreementState.Status.COMPLETED -> throw IllegalArgumentException("Should not be Completed input")
                 }
             }
 
